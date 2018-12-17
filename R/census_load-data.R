04f23ba0ff2c913f6087122b445ef4ba905a2397
Garrick Aden-Buie <garrick.aden-buie@moffitt.org>
2018-12-17 10:39:32 -0500

    clean old code from census_load-data

--- R/census_load-data.R
+++ R/census_load-data.R
@@ -6,22 +6,6 @@ florida_counties <- us_counties(states = "Florida")
 
 
 # Import US Census Data ---------------------------------------------------
-# acs5_popvars <- readRDS(here::here("data", "acs5_popvars.rds"))
-# fl_acs5_county <- readRDS(here::here("data", "fl_acs5_county.rds"))
-# us_acs5 <- readRDS(here::here("data", "us_acs5.rds"))
-#
-# us_acs5_total <-
-#   us_acs5 %>%
-#   group_by(variable, sex, age_group, concept) %>%
-#   summarize(total = sum(estimate))
-
-# fs::dir_ls(here::here("data"), regexp = "dec") %>%
-#   fs::path_rel(here::here()) %>%
-#   map_chr(~ paste0(fs::path_file(fs::path_ext_remove(.x)),
-#                    ' <- readRDS(here::here("',
-#                    fs::path_dir(.x), '", "', fs::path_file(.x), '"))')) %>%
-#   cat(sep = "\n")
-
 dec_2000_popvars <- readRDS(here::here("data", "dec_2000_popvars.rds"))
 fl_dec_2000 <- readRDS(here::here("data", "fl_dec_2000.rds"))
 us_dec_2000 <- readRDS(here::here("data", "us_dec_2000.rds"))
