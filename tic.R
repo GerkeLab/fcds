48822ce2dc19f73bc35de7c1b23ba396125eb831
Garrick Aden-Buie <Garrick.Aden-Buie@moffitt.org>
2019-05-07 08:35:23 -0400

    travis: try again to ensure sf installed

--- tic.R
+++ tic.R
@@ -1,11 +1,14 @@
 do_package_checks()
 
+get_stage("install") %>%
+  add_step(step_install_cran("ggplot2"))
+  add_step(step_install_cran("sf"))
+
+
 if (Sys.getenv("id_rsa") != "" && !ci()$is_tag()) {
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  add_step(get_stage("install"), step_install_cran(c("ggplot2")))
-  add_step(get_stage("install"), step_install_cran(c("sf")))
   do_pkgdown(path = "docs", branch = "gh-pages")
 }
