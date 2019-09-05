d29925ae4f908c30b3caf1b7ec12d871c325bdb4
Garrick Aden-Buie <garrick.aden-buie@moffitt.org>
2019-09-05 14:12:51 -0400

    Add tests for pkg data

--- data-raw/00_make-data.R
+++ data-raw/00_make-data.R
@@ -1,11 +1,22 @@
 fcds:::requires_package(c("callr", "here"), "data importing scripts")
 
+UPDATE_FCDS_EXAMPLE <- FALSE
+
 fs::dir_create(here::here("data"))
 callr::rscript(here::here("data-raw/01_county-fips-florida.R"))
 callr::rscript(here::here("data-raw/02_seer-pop-florida-county.R"))
 callr::rscript(here::here("data-raw/03_seer-pop-us-standard.R"))
 callr::rscript(here::here("data-raw/04_seer-icd-o-3.R"))
-callr::rscript(here::here("data-raw/05_fcds-example.R"))
 
-usaboundaries_counties_fl <- USAboundaries::us_counties(states = "FL")
-usethis::use_data(usaboundaries_counties_fl, internal = TRUE, overwrite = TRUE)
+if (UPDATE_FCDS_EXAMPLE && length(fcds::fcds_cache_ls())) {
+  callr::rscript(here::here("data-raw/05_fcds-example.R"))
+}
+
+# may need to run this next bit manually
+devtools::load_all('.')
+testthat::test_file(here::here("tests/testthat/test-pkg-data.R"))
+
+if (!requireNamespace("USAboundaries", quietly = TRUE)) {
+  usaboundaries_counties_fl <- USAboundaries::us_counties(states = "FL")
+  usethis::use_data(usaboundaries_counties_fl, internal = TRUE, overwrite = TRUE)
+}
---
+++ tests/testthat/test-pkg-data.R
@@ -0,0 +1,27 @@
+context("data checks")
+
+test_that("population data has years matching fcds mid years", {
+  seer_pop_fl_years <- unique(seer_pop_fl$year)
+  seer_pop_fl_1990_years <- unique(seer_pop_fl_1990$year)
+  seer_pop_us_years <- unique(seer_pop_us$year)
+
+  fcds_mid_years <- mid_year(fcds_const("year_group"))
+
+  expect_setequal(seer_pop_fl_years, fcds_mid_years)
+  expect_setequal(seer_pop_fl_1990_years, fcds_mid_years[fcds_mid_years >= "1990"])
+  expect_setequal(seer_pop_us_years, fcds_mid_years[fcds_mid_years >= "1990"])
+})
+
+test_that("county names match", {
+  expect_setequal(county_fips_fl$county_name, fcds_const("county_name"))
+})
+
+test_that("population data didn't change", {
+  expect_known_hash(seer_pop_fl, "35b1ad25ae")
+  expect_known_hash(seer_pop_fl_1990, "2dea1c21c4")
+  expect_known_hash(seer_pop_us, "3caa057477")
+})
+
+test_that("icdo3 data didn't change", {
+  expect_known_hash(seer_icd_o_3, "6003bfea82")
+})
